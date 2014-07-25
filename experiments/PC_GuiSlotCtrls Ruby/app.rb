21777f0e718710aa697f79e8d3ed6aa57948dc0c
mviceral <marvin.viceral@gmail.com>
2014-07-25 11:54:06 -0700

    Had to change it somehow.

--- experiments/PC_GuiSlotCtrls Ruby/app.rb
+++ experiments/PC_GuiSlotCtrls Ruby/app.rb
@@ -48,36 +48,6 @@ module MigrationCount
 		# the version
 		#
 		namespace :migrations do
-			
-			# GET /vi/migrations/cars
-			get "/cars" do
-				puts "Migrations.quantity: #{Migrations.quantity.inspect}" if $debug
-
-
-				db = SQLite3::Database.open "latest.db"
-				db.results_as_hash = true
-
-				ary = db.execute "SELECT * FROM Cars"    
-				asdf = []
-				ary.each do |row|
-					fetchedData = {
-					:Id => row['Id'],
-					:Name => row['Name'], 
-					:Price => row['Price']
-					}
-					asdf.push(fetchedData)
-				end
-
-				#rescue SQLite3::Exception => e 
-				#    puts "Exception occured"
-				#    puts e
-
-				#ensure
-				#    db.close if db
-				#
-				asdf 
-			end
-
 			# POST /vi/migrations/Duts
 			# If you supply an integer parameter in the body or the url named 'Id'
 			# you will get the record with that id.
@@ -88,7 +58,9 @@ module MigrationCount
 					# Parse out the data sent from BBB
 					#
 					receivedData = params['Duts']
-					# puts "receivedData = #{receivedData}" 
+					# puts "1 receivedData = #{receivedData}" 
+					receivedData = receivedData.partition("BBB")[2]
+					# puts "2 receivedData = #{receivedData}" 
 					timeOfData = receivedData.partition("|")
 					dutData = timeOfData[2]
 
@@ -113,24 +85,6 @@ module MigrationCount
 					dbRecord.execute "#{str}"					
 					{dataTime:timeOfData[0]}
 				end
-			end
-
-
-			get "/:name" do
-				# matches "GET /hello/foo" and "GET /hello/bar"
-				# params[:name] is 'foo' or 'bar'
-				str = "<table><row></row></table>Hello #{params[:name]}!"
-				formatter str: HtmlFormatter 
-			end
-			
-			# POST /vi/migrations/inc
-			# If you supply an integer parameter in the body or the url named 'value'
-			# it will add that ingeter to the current count
-			# If you don't supply a parameter it increments the count by 1
-			#
-			post "/submit" do
-				html = 'You entered "'+params['data']+'"'
-				return html
 			end
 		end
 	end		
