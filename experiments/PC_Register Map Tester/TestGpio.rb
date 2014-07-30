a074f4f363bbeb354bb457a9b4eac84ffd0ec90c
mviceral <marvin.viceral@gmail.com>
2014-07-30 13:37:19 -0700

    Updated file again.

--- experiments/PC_Register Map Tester/TestGpio.rb
+++ experiments/PC_Register Map Tester/TestGpio.rb
@@ -200,39 +200,46 @@ def uiTest
 
 	<div id=\"myDiv\">
 	<center>
-	<table id=\"main\" width=\"100%\" style=\"border: 1px solid black;\">
-		<tr id=\"main\">
-			<td id=\"main\" width=\"5%\" ></td>
-			<td id=\"main\" width=\"5%\" ></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">7</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">6</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">5</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">4</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">3</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">2</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">1</font></center></td>
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">0</font></center></td>			
-			<td id=\"main\" width=\"3%\" ><center><font size=\"1\">HEX</font></center></td>			
-		</tr>"
-		ui += readItemBit("0x00 SLOTADDR","SLOT1|SLOT0|SYS5|SYS4|SYS3|SYS2|SYS1|SYS0")
-		ui += testItemBit("0x00 RESET_ALL","RST")	
-		ui += testItemBit("0x01 STAT_LED","LEDEN| | | |LED3|LED2|LED1|LED0")	
-		ui += testItemBit("0x02 WXT_CLEAR_LATCH","CLEAR")
-		ui += readItemBit("0x02 EXT_INPUTS","FANT2B|FANT2A|FANT1B|FANT1A|SENSR2|SENSR1|USRSW2|USRSW1")
-		ui += testItemBit("0x03 PS_ENABLE","P12V|N5V|P5V|PS6|PS8|PS9|PS10")	
-		ui += testItemBit("0x04 SL_CNTL_EXT","POWER| |FAN1|FAN2|BUZR|LEDRED|LEDYEL|LEDGRN")	
-		ui += testItemByte("0x05 SL_FAN_PWM","PWM7|PWM6|PWM5|PWM4|PWM3|PWM2|PWM1|PWM0")
-		ui += readItemBit("0x06 ETS_ALM1","ALM7|ALM6|ALM5|ALM4|ALM3|ALM2|ALM1|ALM0")
-		ui += readItemBit("0x07 ETS_ALM2","ALM15|ALM14|ALM13|ALM12|ALM11|ALM10|ALM9|ALM8")
-		ui += readItemBit("0x08 ETS_ALM2","ALM23|ALM22|ALM21|ALM20|ALM19|ALM18|ALM17|ALM16")
-		ui += testItemBit("0x09 ETS_ENA1","ETS7|ETS6|ETS5|ETS4|ETS3|ETS2|ETS1|ETS0")
-		ui += testItemBit("0x0A ETS_ENA2","ETS15|ETS14|ETS13|ETS12|ETS11|ETS10|ETS9|ETS8")
-		ui += testItemBit("0x0B ETS_ENA3","ETS23|ETS22|ETS21|ETS20|ETS19|ETS18|ETS17|ETS16")
-		ui += testItemByte("0x0C ETS_RX_SEL","RXMUX4|RXMUX3|RXMUX2|RXMUX1|RXMUX0")
-		ui += testItemByte("0x0D ANA_MEAS4_SEL","ANAMUX5|ANAMUX4|ANAMUX3|ANAMUX2|ANAMUX1|ANAMUX0")
-		ui += 
-	"</table>
-        		"
+		<table>
+			<tr><td>&nbsp;</td><td><center><font size=\"3\">Application Test Panel</font></center></td><td>&nbsp;</td></tr>
+			<tr><td>&nbsp;</td><td><table id=\"main\" width=\"100%\" style=\"border: 1px solid black;\">
+			<tr id=\"main\">
+				<td id=\"main\" width=\"5%\" ></td>
+				<td id=\"main\" width=\"5%\" ></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 7</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 6</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 5</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 4</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 3</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 2</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 1</font></center></td>
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">Bit 0</font></center></td>			
+				<td id=\"main\" width=\"3%\" ><center><font size=\"1\">HEX</font></center></td>			
+			</tr>"
+			ui += readItemBit("0x00 SLOTADDR","SLOT1|SLOT0|SYS5|SYS4|SYS3|SYS2|SYS1|SYS0")
+			ui += testItemBit("0x00 RESET_ALL","RST")	
+			ui += testItemBit("0x01 STAT_LED","LEDEN| | | |LED3|LED2|LED1|LED0")	
+			ui += testItemBit("0x02 WXT_CLEAR_LATCH","CLEAR")
+			ui += readItemBit("0x02 EXT_INPUTS","FANT2B|FANT2A|FANT1B|FANT1A|SENSR2|SENSR1|USRSW2|USRSW1")
+			ui += testItemBit("0x03 PS_ENABLE","P12V|N5V|P5V|PS6|PS8|PS9|PS10")	
+			ui += testItemBit("0x04 SL_CNTL_EXT","POWER| |FAN1|FAN2|BUZR|LEDRED|LEDYEL|LEDGRN")	
+			ui += testItemByte("0x05 SL_FAN_PWM","PWM7|PWM6|PWM5|PWM4|PWM3|PWM2|PWM1|PWM0")
+			ui += readItemBit("0x06 ETS_ALM1","ALM7|ALM6|ALM5|ALM4|ALM3|ALM2|ALM1|ALM0")
+			ui += readItemBit("0x07 ETS_ALM2","ALM15|ALM14|ALM13|ALM12|ALM11|ALM10|ALM9|ALM8")
+			ui += readItemBit("0x08 ETS_ALM2","ALM23|ALM22|ALM21|ALM20|ALM19|ALM18|ALM17|ALM16")
+			ui += testItemBit("0x09 ETS_ENA1","ETS7|ETS6|ETS5|ETS4|ETS3|ETS2|ETS1|ETS0")
+			ui += testItemBit("0x0A ETS_ENA2","ETS15|ETS14|ETS13|ETS12|ETS11|ETS10|ETS9|ETS8")
+			ui += testItemBit("0x0B ETS_ENA3","ETS23|ETS22|ETS21|ETS20|ETS19|ETS18|ETS17|ETS16")
+			ui += testItemByte("0x0C ETS_RX_SEL","RXMUX4|RXMUX3|RXMUX2|RXMUX1|RXMUX0")
+			ui += testItemByte("0x0D ANA_MEAS4_SEL","ANAMUX5|ANAMUX4|ANAMUX3|ANAMUX2|ANAMUX1|ANAMUX0")
+			ui += 
+		"</table></td><td>&nbsp;</td></tr>
+			<tr>
+				<td>&nbsp;</td><td><center><table id=\"main\"><tr id=\"main\" bgcolor=\"#33dd66\"><td id=\"main\" width=\"10%\"><center><font size=\"2\">App response</font></center></td><td id=\"main\" width=\"90%\"><font size=\"2\">App response sample</font></td></tr></table></center></td><td>&nbsp;</td></tr>
+			<tr><td>&nbsp;</td></tr>
+		</table>
+	"
+	ui += ""
 	ui += "</div>	
 	</center>
 	<script type=\"text/javascript\">
