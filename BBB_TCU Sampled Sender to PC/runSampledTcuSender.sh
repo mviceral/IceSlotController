bf3e021dea65a742a0d1d956b45902babe88acfe
root <root@beaglebone.(none)>
2014-08-04 14:40:06 +0000

    New bash file for running the sampled Tcd sender ruby script.

---
+++ BBB_TCU Sampled Sender to PC/runSampledTcuSender.sh
@@ -0,0 +1,17 @@
+#
+# Make sure only one process is running for the TCU Sampler.
+#
+LockFile=/tmp/bbbSampledTcuSenderLock.txt
+if [ -e ${LockFile} ] && kill -0 `cat ${LockFile}`; then
+    echo "runSampledTcuSender.sh is already running"
+    exit
+fi
+
+# make sure the lockfile is removed when we exit and then claim it
+trap "rm -f ${LockFile}; exit" INT TERM EXIT
+echo $$ > ${LockFile}
+
+# do stuff
+ruby SendSampledTcuToPc.rb
+
+rm -f ${LockFile}
