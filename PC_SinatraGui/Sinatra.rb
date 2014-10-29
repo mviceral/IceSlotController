c3b094eda39b538d424f7b9d45568c305cc8b723
mviceral <marvin.viceral@gmail.com>
2014-10-29 13:49:32 -0700

    Latest.

--- PC_SinatraGui/Sinatra.rb
+++ PC_SinatraGui/Sinatra.rb
@@ -15,11 +15,6 @@ require 'drb/drb'
 
 class UserInterface
 	SERVER_URI="druby://localhost:8787"
-	# BbbPcListener = 'http://192.168.1.212'
-	# Slot1IpAddress  = 'http://192.168.7.2'
-	# BbbPcListener = 'http://192.168.1.211'
-	# LinuxBoxPcListener = "localhost"
-	# PcListener = Slot1IpAddress  # Chose which ethernet address the PcListener is sitting on.
 	#
 	# Template flags
 	#
@@ -94,8 +89,8 @@ class UserInterface
 	def getBoardIp(slotParam, fromParam)
 		if @slotToIp.nil? # || @slotToIp[slotParam].nil? ||@slotToIp[slotParam].length == 0
 			@slotToIp = Hash.new
-			@slotToIp[SharedLib::SLOT1] = "192.168.1.211"
-			#@slotToIp[SharedLib::SLOT2] = "192.168.1.212"
+			#@slotToIp[SharedLib::SLOT1] = "192.168.1.211"
+			@slotToIp[SharedLib::SLOT2] = "192.168.1.212"
 			#@slotToIp[SLOT3] = ""
 		end
 		# puts "slotParam='#{slotParam}' @slotToIp='#{@slotToIp}' fromParam=#{fromParam} #{__LINE__}-#{__FILE__}"
