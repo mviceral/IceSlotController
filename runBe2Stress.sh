3d6a0c83701ea8138963ec3588e91f32307f7705
Centos Seven <cseven@localhost.localdomain>
2014-11-12 10:46:34 -0800

    Latest.

--- runBe2Stress.sh
+++ runBe2Stress.sh
@@ -1,12 +1,11 @@
 # This script will run the processes for the BE2STRESS.
-clear
-echo "A browser will open a page 'http://localhost:4569' to view the BE2 application." 
+# clear
+# echo "A browser will open a page 'http://localhost:4569' to view the BE2 application." 
 #echo "Processing. Please wait ~1 min."
 bash scripts/pcSideRun.sh 2>/dev/null &
 ssh root@192.168.1.211 'bash -s' < scripts/sshScriptRunBoard.sh 2>/dev/null & 
 ssh root@192.168.1.212 'bash -s' < scripts/sshScriptRunBoard.sh 2>/dev/null & 
 #sleep 60  
 xdg-open http://localhost:4569 &
-exit
-#clear
-#echo "Script complete.  BE2 application can be accessed through a browser at 'http://localhost:4569'."
+clear
+echo "BE2 start-up script complete.  BE2 application can be accessed through a browser at 'http://localhost:4569'."
