2232f61b7b4124c4614718089e0ecbe6122a676c
Centos Seven <cseven@localhost.localdomain>
2014-11-12 10:50:05 -0800

    Latest.

--- runBe2Stress.sh
+++ runBe2Stress.sh
@@ -9,3 +9,4 @@ ssh root@192.168.1.212 'bash -s' < scripts/sshScriptRunBoard.sh 2>/dev/null &
 xdg-open http://localhost:4569 &
 clear
 echo "BE2 start-up script complete.  BE2 application can be accessed through a browser at 'http://localhost:4569'."
+echo "Once the the display values on the slots are active, they're ready for commands."
