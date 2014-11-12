0c14fd9e18ee3f2685d9e7a195473ab8bc8973da
Centos Seven <cseven@localhost.localdomain>
2014-11-12 15:52:32 -0800

    Latest.

--- runBe2Stress.sh
+++ runBe2Stress.sh
@@ -9,7 +9,7 @@ ssh root@192.168.1.212 'bash -s' < scripts/sshScriptRunBoard.sh 2>/dev/null &
 sleep 3 # wait for the noise from the top processes to show then clear the screen.
 clear
 echo "BE2 start-up script complete.  BE2 application can be accessed through a browser at 'http://localhost:4569'."
-echo "Please wait for the page to load since the browser is waiting for the slot controllers to respond."
+echo "Please wait for the page to load since the browser is waiting for the slot controllers to respond (at most, about ~1 minute)."
 echo "Once the the display values on the slots are active, they're ready for commands."
 read -n1 -r -p "Press any to continue..." key
 
